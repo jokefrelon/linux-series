0187d4e0f327688704ed6622c0e4b3206b784e58
JokeMe <lixuanliming@gmail.com>
2019-07-04 22:53:54 +0800

    Robot auto update

--- getip.sh
+++ getip.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
-a=10
-b=10
-if [$a==$b]
-	then
-		ifconfig >/mnt/github/shell/ipadder.txt
-		cat ipadder.txt
-	else
-		echo "Please checkout the shell"
-	fi
-cd /mnt/github/shell/
-git add ipadder.txt 
-git commit -m "Robot auto update"
-git push getip master
-sleep 3600
-cd /mnt/github/shell/
-./getip.sh
-
+# a=10
+# b=10
+# if [$a==$b]
+# 	then
+# 		ifconfig > /mnt/github/shell/ipadder.txt
+# 		cat ipadder.txt
+# 	else
+# 		echo "Please checkout the shell"
+# 	fi
+while  true
+do
+    cd /mnt/github/shell/
+    ifconfig  > ipadder.txt
+    cat ipadder.txt
+    git add ipadder.txt 
+    git commit -m "Robot auto update"
+    git push getip master
+    sleep 3600
+    cd /mnt/github/shell/
+    ./getip.sh
+done
