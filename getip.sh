41aa48ea1a8b73f0fda65300356b872deafea234
root <root@OpenWrt.lan>
2019-07-07 18:38:09 +0800

    Robot auto update

--- getip.sh
+++ getip.sh
@@ -12,7 +12,8 @@ while  true
 do
     cd /mnt/github/shell/
     ifconfig | grep 172.17  > ipadder.txt
-    cat ipadder.txt
+#    cat ipadder.txt
+    date >> ipadder.txt
     git add ipadder.txt getip.sh 
     git commit -m "Robot auto update"
     git push getip master
