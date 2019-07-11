c256376574e5808fa2f5796648848440294f38c8
root <root@OpenWrt.lan>
2019-07-11 17:47:29 +0800

    Robot auto update

--- getip.sh
+++ getip.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-a=10
-b=10
-if [$a==$b]
+$a=1
+$b=10
+if [$a<$b]
 	then
     		cd /mnt/github/shell/
     		ifconfig | grep 172.17  > ipadder.txt
@@ -12,6 +12,4 @@ if [$a==$b]
     		echo -e "\033[31m| This program will sleep six minutes |\033[0m"
     		echo -e "\033[31m---------------------------------------\033[0m"
 		sleep 6m
-	else
-		then
-			break
+fi
