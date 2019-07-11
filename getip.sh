6a6d3010722db678eb9690b9ddb3a17d83e56e15
root <root@OpenWrt.lan>
2019-07-11 17:59:41 +0800

    Robot auto update

--- getip.sh
+++ getip.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
-a=1
-b=10
-if [$a<$b]
-	then
-    		cd /mnt/github/shell/
-    		ifconfig | grep 172.17  > ipadder.txt
-    		git add ipadder.txt getip.sh README.md
-    		git commit -m "Robot auto update"
-    		git push getip master
-    		echo -e "\033[31m---------------------------------------\033[0m"
-    		echo -e "\033[31m| This program will sleep six minutes |\033[0m"
-    		echo -e "\033[31m---------------------------------------\033[0m"
-		sleep 6m
+ab=10
+bc=10
+if [$ab==$bc]
+then
+	echo " hello "
+	cd /mnt/github/shell/
+    	ifconfig | grep 172.17  > ipadder.txt
+    	git add ipadder.txt getip.sh README.md
+    	git commit -m "Robot auto update"
+    	git push getip master
+    	echo -e "\033[31m---------------------------------------\033[0m"
+    	echo -e "\033[31m| This program will sleep six minutes |\033[0m"
+    	echo -e "\033[31m---------------------------------------\033[0m"
+	sleep 6m
 fi
